51b85ddb2122ec3d65e9a8554df4bf70a1a746ec
snau <shimmy.nauenberg@mail.utoronto.ca>
2020-11-10 12:12:09 -0500

    commit preliminary OrganizerManager

--- phase1/src/Person/Organizer.java
+++ phase1/src/Person/Organizer.java
@@ -2,7 +2,7 @@ package Person;
 
 public class Organizer extends Attendee {
     Organizer(String fullName, String username, String password, String email){
-        super(fullName, username, password, email );
+        super(fullName, username, password, email);
     }
 
 
--- phase1/src/Person/OrganizerManager.java
+++ phase1/src/Person/OrganizerManager.java
@@ -1,4 +1,38 @@
 package Person;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class OrganizerManager extends PersonManager {
+    private ArrayList<Organizer> organizerList;
+    private Organizer currentOrganizer;
+    private Map usernameToOrganizer;
+    OrganizerManager(String organizerName){
+        organizerList  = new ArrayList<Organizer>();
+    }
+    public boolean createOrganizer(){ //waiting for builder add organizer to list update map
+        return true;
+    }
+
+    public ArrayList<Organizer> getAttendeeList() {
+        return organizerList;
+    }
+
+    public boolean createSpeaker() { // waiting for person factory
+        return true;
+    }
+    public boolean updateEventSignups(){ // updates the events the current organizer is signed up fro
+
+    }
+    public String getAttribute(String attribute){ // see if there is a way to be able to do this.
+
+    }
+
+    public void updateCurrentOrganizer(String Username){ // use the map to update the person
+
+    }
+    public void updateContactList(String Username){
+        currentOrganizer.getContactList().add(Username);
+    }
+
 }
