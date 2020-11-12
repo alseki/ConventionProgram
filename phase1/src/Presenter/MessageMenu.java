459c3c71260a0ef8aad36cb73fb783520125d2d4
pikar <karyn.komatsu@mail.utoronto.ca>
2020-11-12 09:25:13 -0500

    Coded Tests for Message package. Modification are to be made after meeting on Nov. 12th at 4pm EST. Improving on MessageMenu.

--- phase1/src/Presenter/MessageMenu.java
+++ phase1/src/Presenter/MessageMenu.java
@@ -3,11 +3,26 @@ package Presenter;
 // Programmers: Cara McNeil, Karyn Komatsu
 // Description: Prints information pertaining to a user's message information
 // Date Created: 11/11/2020
-// Date Modified: 11/12/2020
+// Date Modified: 12/11/2020
 
 public class MessageMenu {
 
     /**
-     *
+     * Prompts user to print Message.
+     * @return True iff the Message was properly formatted and printed.
      */
+    public boolean printFormattedMessage(){
+        System.out.println("Print Message: \n");
+
+        return true;
+    }
+
+    /**
+     * Prompts user to print Chat.
+     * @return True iff the Chat was properly printed.
+     */
+    public boolean printChat(){
+        return true;
+    }
+    //TO BE FIXED (Will clarify how we'd want to print the Chat. In what format etc.
 }
