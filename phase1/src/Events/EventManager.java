28ab3cc33f5328203e4aeb7f0bef028c3c913da8
Sarah <emerald1311@gmail.com>
2020-11-14 13:09:32 -0500

    Cleaned up EventManager

--- phase1/src/Events/EventManager.java
+++ phase1/src/Events/EventManager.java
@@ -15,54 +15,62 @@ public class EventManager {
    // EventAccessManager eventAccess;
    // EventAdminManager eventAdmin;
     EventDB events;
-    EventReader reader;
-
-
-    /**
-     * Creates a new EventManager with events read in by some sort of EventReader
-     * @param reader The EventReader used to read in events
-     */
-    public EventManager(EventReader reader) {
-        Room thisRoom = new Room();
-        this.reader = reader;
-        this.rmBuilder(reader, thisRoom);
-    }
-
+    Room room;
 
     /**
      * Creates a new EventManager with events read in by some sort of EventReader
-     * @param reader The EventReader used to read in events
+     * @param room The EventReader used to read in events
      */
     public EventManager(Room room) {
-        // AAAAAAH?
+        this.room = room;
     }
 
-    /**
-     * Creates a new EventManager with events read in by some sort of EventReader
-     * @param reader The EventReader used to read in events
-     * @param name The name of this room
-     */
-    public EventManager(EventReader reader, String name) {
-        Room thisRoom = new Room(name);
-        this.reader = reader;
-        this.rmBuilder(reader, thisRoom);
-    }
+    
 
-    private void rmBuilder(EventReader reader, Room room) {
-        events = new EventDB(reader.readEvents(), room);
-        //eventSignup = new EventSignupManager(events);
-        //eventAccess = new EventAccessManager(events);
-        //eventAdmin = new EventAdminManager(events);
-    }
 
     /**
      * Returns information about the entire list of events
      * @return The list of events
      */
     public Event[] getEventList() {
-        Event[] eventArray = {};
         return events.getEventList();
     }
+
+    /**
+     * Returns the ID of an event given its name
+     * @param name The event's name
+     * @return The ID
+     */
+    public String getEventID(String name) {
+        return events.getEventID(name);
+    }
+
+
+
+
+    /**
+     * Adds an event
+     * @param event The event
+     * @return Whether the event has been successfully added
+     */
+    public boolean addEvent(Event event) {
+
+        // check conflicts! also possibly edit so as to create the event itself?
+
+        return events.addEvent(event);
+    }
+
+    /**
+     * Deletes an event
+     * @param id The event's ID
+     * @return Whether the event has been successfully deleted
+     */
+    public boolean removeEvent(String id) {
+        return events.addEvent(events.getEvent(id));
+    }
+
+
+
 
     /**
      * Signs an individual attendee up for an event
@@ -101,45 +109,13 @@ public class EventManager {
         }
     }
 
-    /**
-     * Returns the ID of an event given its name
-     * @param name The event's name
-     * @return The ID
-     */
-    public String getEventID(String name) {
-        return events.getEventID(name);
-    }
 
-    /**
-     * Deletes an event
-     * @param id The event's ID
-     * @return Whether the event has been successfully deleted
-     */
-    public boolean removeEvent(String id) {
-        return events.addEvent(events.getEvent(id));
-    }
 
-    /**
-     * Adds an event
-     * @param event The event
-     * @return Whether the event has been successfully added
-     */
-    public boolean addEvent(Event event) {
-
-        // check conflicts! also possibly edit so as to create the event itself?
-
-        return events.addEvent(event);
-    }
-
-    public void saveRoom() {
-        reader.save(getEventList());
+    public boolean announceToEvent(Event event) {
+        return false; //get messages
     }
 
     public MessageManager getMessages(Event event) {
         return null; //get messages
-    }
-
-    public boolean announceToEvent(Event event) {
-        return false; //get messages
     }
 }
