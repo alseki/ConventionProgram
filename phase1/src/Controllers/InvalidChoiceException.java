0e22a53a70456a92260247aeea51e859d071e85b
Sarah <emerald1311@gmail.com>
2020-11-17 12:17:22 -0500

    Committing InvalidChoiceException
    Can be thrown for any choice not on a list of acceptable choices

---
+++ phase1/src/Controllers/InvalidChoiceException.java
@@ -0,0 +1,18 @@
+package Events;
+
+public class InvalidChoiceException extends Exception {
+    String type;
+
+    public InvalidChoiceException(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Whoops! That's an invalid " + type + " choice. Please try again!";
+    }
+
+    public void printErrorMessage() {
+        System.out.println(getMessage());
+    }
+}
