479a38772d9bdc7c579a73ffab03cf3f0b41effe
yiran <r.yi@mail.utoronto.ca>
2020-11-18 06:19:52 -0500

    SpeEventController, SpeEventMenu: a rough work

--- phase1/src/Presenter/SpeEventMenu.java
+++ phase1/src/Presenter/SpeEventMenu.java
@@ -10,16 +10,44 @@ public class SpeEventMenu implements printSubMenu {
     @Override
     public void printMenuOptions() {
         System.out.println("----- Speaker Event Menu -----");
-        System.out.println("To return to Main Menu, Enter '0'.");
+        System.out.println("Return to Main Menu -------------------------------- Enter '0'.");
+        System.out.println("View the talk you're going to talk at -------------- Enter '1'.");
+        System.out.println("Send message to all Users in an Event -------------- Enter '2'.");
+        System.out.println("Send message to all Attendees in an Event ---------- Enter '3'.");
+        System.out.println("Send message to all Attendees in your Events ------- Enter '4'.");
+        System.out.println("to be finished ------------------------ Enter '5'.");
+        System.out.println("to be finished ----- Enter '6'.");
+        System.out.println("Send message to one Attendee in an Event ----------- Enter '7'.");
         // TODO add print statements for all the other menu options
         
     }
 
     /**
-     * Prompts user to print Talks.
+     * Print String.
      */
-    public void printFormattedTalks(ArrayList<String> eventIds) {
-        System.out.println("Print Talks: \n");
-        }
+    public void printString(String str) {
+        System.out.println("str");
+    }
+
+    /**
+     * Prompts user to enter eventName of the event want to send message to.
+     */
+    public void printEventNamePrompt() {
+        System.out.println("Enter the NAME of the Event you want to send message to.");
+    }
+
+    /**
+     * Prompts user to enter CONTENT of the message.
+     */
+    public void printContentPrompt() {
+        System.out.println("Enter the CONTENT of the message.");
+    }
+
+    /**
+     * Tell the User the message is sent.
+     */
+    public void printMessageSent() {
+        System.out.println("Messages are sent!");
+    }
 
 }
