0f3f9125de27ce16351c9e62c8fd474444dac79d
alsek <allenseung.kim@mail.utoronto.ca>
2020-12-02 16:51:48 -0500

    created AdminManager

---
+++ phase2/src/Person/AdminManager.java
@@ -0,0 +1,12 @@
+package Person;
+
+import java.util.ArrayList;
+
+public class AdminManager {
+
+    ArrayList<String> allAdmins;
+
+    public AdminManager(ArrayList<String> admins) {
+        allAdmins = admins;
+    }
+}
