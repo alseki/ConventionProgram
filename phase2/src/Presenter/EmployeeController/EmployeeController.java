968a09efa1e6df0622c6492db414771626e9bd97
PaulThomas <vikram.thomas@mail.utoronto.ca>
2020-12-04 20:31:11 -0500

    Employee controller; scrapping idea of EmpEventController

--- phase2/src/Presenter/EmployeeController/EmployeeController.java
+++ phase2/src/Presenter/EmployeeController/EmployeeController.java
@@ -2,7 +2,7 @@ package Presenter.EmployeeController;
 
 // Description: Main account page for EmployeeController users.
 
-RETURN controllers; no void methods
+// RETURN controllers; no void methods
 
 import Event.EventManager;
 import Event.RoomManager;
@@ -10,10 +10,12 @@ import Message.ChatManager;
 import Message.MessageManager;
 import Person.EmployeeManager;
 import Presenter.Central.SubMenu;
+import Presenter.PersonController.ContactController;
 import Presenter.PersonController.MessageController;
 import Presenter.PersonController.PersonController;
-import Presenter.RequestController.RequestController;
 import Request.RequestManager;
+import Request.controllers.EmpReqController;
+
 
 // create request
 // work with constraints
@@ -37,72 +39,23 @@ public class EmployeeController extends PersonController {
                 return new MessageController(this, currentUserID);
             }
             else if (choice.equals(options[1])) {
-                return new RequestController(this, currentUserID);
+                return new ContactController(this, currentUserID);
             }
-            else if (choice.equals(options[2])) {
-                return new EmpEventController(this, currentUserID);
+            else if (choice.equals(options[2])){
+                return new EmpReqController(this, currentUserID);
             }
         }
         return null;
     }
 
+    @Override
+    public String[] getMenuOptions() {
+        String[] employeeOptions  = {"View the request board"};
+        String[] options = new String[3];
+        System.arraycopy(super.getMenuOptions(), 0, options, 0, 2);
+        System.arraycopy(employeeOptions, 0, options, 2, 1);
+        return options;
+    }
+
 }
-//// Programmer: Cara McNeil, Sarah Kronenfeld
-////
-//// Date Created: 01/11/2020
-//// Date Modified: 19/11/2020
-//
-//
-//import Presenter.AttendeeController.AttEventController;
-//        import Presenter.AttendeeController.AttMessageController;
-//        import Presenter.AttendeeController.AttReqController;
-//        import Presenter.Central.SubMenu;
-//        import Presenter.PersonController.ContactController;
-//        import Presenter.PersonController.MessageController;
-//        import Presenter.PersonController.PersonController;
-//        import Event.EventManager;
-//        import Event.RoomManager;
-//        import Message.ChatManager;
-//        import Message.MessageManager;
-//        import Person.SpeakerManager;
-//        import Request.RequestManager;
-//
-//public class SpeakerController extends PersonController {
-//    private SpeakerManager manager;
-//
-//    public SpeakerController(SpeakerManager manager, RoomManager rooms, EventManager events,
-//                             MessageManager messages, ChatManager chats, RequestManager requests) {
-//        super(manager, rooms, events, messages, chats, requests, 3);
-//        this.manager = manager;
-//    }
-//
-//    /**
-//     * Creates the next controller according to the user's menu choice
-//     */
-//    @Override
-//    public SubMenu createController(String choice) {
-//        String[] options = getMenuOptions();
-//        if (super.loggedIn) {
-//            if (choice.equals(options[0])) {
-//                return new ContactController(this, currentUserID);
-//            }
-//            else if (choice.equals(options[1])) {
-//                return new MessageController(this, currentUserID);
-//            }
-//            else if (choice.equals(options[2])) {
-//                return new SpeEventController(this, manager, currentUserID);
-//            }
-//        }
-//        return null;
-//    }
-//
-//    @Override
-//    public String[] getMenuOptions() {
-//        String[] attendeeOptions  = {"View your event information"};
-//        String[] options = new String[3];
-//        System.arraycopy(super.getMenuOptions(), 0, options, 0, 2);
-//        System.arraycopy(attendeeOptions, 0, options, 2, 1);
-//        return options;
-//    }
-//
-//}
+
