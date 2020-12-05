169e8d71ed54bd63a3feb8a8f5f930eb6e7b3a58
take-cara-yourself <caramcneil12@gmail.com>
2020-12-05 00:39:19 -0500

    Added controller methods into Contact View

--- phase2/src/Presenter/PersonController/ContactController.java
+++ phase2/src/Presenter/PersonController/ContactController.java
@@ -3,11 +3,10 @@ package Presenter.PersonController;
 // Programmer: Cara McNeil
 // Description: All the methods that take user input in the Contact Menu
 // Date Created: 01/11/2020
-// Date Modified: 14/11/2020
-
-
+// Date Modified: 04/12/2020
 
 import Presenter.Central.SubMenu;
+import Presenter.Central.SubMenuPrinter;
 import Presenter.Exceptions.InvalidChoiceException;
 
 import java.util.ArrayList;
@@ -23,35 +22,6 @@ public class ContactController extends SubMenu {
     }
 
     /**
-     * Takes user input and calls appropriate methods, until user wants to return to Main Menu
-     */
-    public void menuChoice() {
-        do {
-            switch (1) {
-                case 0:
-                    // return to main menu
-                    break;
-                case 1:
-                    try {
-                        getContactList();
-                    } catch (InvalidChoiceException e) {
-                        presenter.printException(e);
-                    }
-                    break;
-                case 2:
-                    presenter.printAddContactPrompt();
-                    try {
-                        addContact("");//SubMenu.readInput(input));
-                    } catch (InvalidChoiceException e) {
-                        presenter.printException(e);
-                    }
-                    break;
-            }
-        }
-        while (true);
-    }
-
-    /**
      * Get's the current user's contactList
      */
     public String[] getContactList() throws InvalidChoiceException {
@@ -62,6 +32,7 @@ public class ContactController extends SubMenu {
     /**
      * Add a contact to the current user's contactList
      * @param contactUsername the username of the contact the current user wants to add to their contactList
+     * @return a String confirming the contact was added if successful; otherwise return null
      */
     public String addContact(String contactUsername) throws InvalidChoiceException{
         String contactID = personManager.getCurrentUserID(contactUsername);
@@ -77,4 +48,8 @@ public class ContactController extends SubMenu {
      return null;
     }
 
+    @Override
+    public ContactMenu getPresenter() {
+        return this.presenter;
+    }
 }
