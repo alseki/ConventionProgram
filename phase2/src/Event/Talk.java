257677bdbabc44355034bfd1b541129af8c8bb0a
weinst38 <eytan.weinstein@mail.utoronto.ca>
2020-12-05 23:54:47 -0500

    Modifications to classes to allow Speakers to be an optional feature of an Event.

--- phase2/src/Event/Talk.java
+++ phase2/src/Event/Talk.java
@@ -3,33 +3,53 @@ package Event;
 import java.time.LocalDateTime;
 
 // Contributors: Sarah Kronenfeld, Eytan Weinstein
-// Last edit: Nov 17 2020
+// Last edit: Dec 5 2020
 
 // Architecture Level - Entity
 
 public class Talk extends Event {
 
-    // A Talk is exactly one hour long, and cannot take place in the same Room as any other Event while that Event is
-    // occurring.
+    // A Talk is a presentation given by one Speaker, and cannot take place in the same Room as any other Event while
+    // that Event occurring.
+
+    private String speakerID;
 
     /**
      * Constructor for Talk objects
      * @param name The Talk's name
-     * @param speakerID The ID of the speaker at this Talk
+     * @param speakerID The ID of the Speaker at this Talk
      * @param startTime The time when the Talk starts
+     * @param endTime The time when the Talk ends
      * @param description The description of this Talk
      */
-    public Talk (String name, String speakerID, LocalDateTime startTime, String description) {
-        super(name, startTime,startTime.plusHours(1), description, speakerID);
+    public Talk (String name, String speakerID, LocalDateTime startTime, LocalDateTime endTime, String description) {
+        super(name, startTime, endTime, description);
+        this.speakerID = speakerID;
     }
 
     /**
      * Checks if the inputted Event conflicts in time with this Talk
-     * @param event The event which is being checked for conflicts with this Talk
+     * @param event The Event which is being checked for conflicts with this Talk
      * @return whether or not there is a conflict (true or false)
      */
     public boolean conflictsWith(Event event) {
         return this.getStartTime().isBefore(event.getEndTime()) && event.getStartTime().isBefore(this.getEndTime());
+    }
+
+    /**
+     * Getter for the ID of the Speaker at this Talk
+     * @return the ID of the Speaker at this Talk (as a String)
+     */
+    public String getSpeakerID() {
+        return this.speakerID;
+    }
+
+    /**
+     * Setter for the ID of the Speaker at this Talk
+     * @param speakerID The new ID of the Speaker at this Talk (as a String)
+     */
+    public void setSpeakerID(String speakerID) {
+        this.speakerID = speakerID;
     }
 
 }
