5a2aa084326ad6adb1a2979235caacb081069dd0
alsek <allenseung.kim@mail.utoronto.ca>
2020-12-08 20:16:18 -0500

    set up GUI for AttReqView

--- phase2/src/View/Account/AttReqView.java
+++ phase2/src/View/Account/AttReqView.java
@@ -1,21 +1,69 @@
 package View.Account;
 
-import Presenter.AttendeeController.AttEventController;
+import Presenter.AttendeeController.AttReqController;
+import Presenter.AttendeeController.AttReqMenu;
 import Presenter.Central.SubMenu;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class AttReqView extends AccountView {
-    AttEventController controller;
+    AttReqController controller;
+    AttReqMenu presenter;
+    String[] menuOp;
 
     public AttReqView(SubMenu controller) {
         super(controller.getPresenter());
-        this.controller = (AttEventController) controller;
+        this.controller = (AttReqController) controller;
+        this.presenter = ((AttReqController) controller).getPresenter();
+
+        menuOp = this.presenter.getMenuOptions();
+
+        contentPane.setBackground(new Color(100, 200, 232));
     }
+
+
+    private void showMakeReq() {
+
+    }
+
+    private void showViewReqInfo() {
+
+    }
+
+    private void showMyReq() {
+
+    }
+
+    private void modifyReq() {
+
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent event) {
+        super.actionPerformed(event);
+        String eventName = event.getActionCommand();
 
+        // [1] = make a request
+        // [2] = view info about a request
+        // [3] = show my requests
+        // [4] = modify a request
+        if(eventName.equals(menuOp[1])) {
+            showMakeReq();
+        }
+
+        if(eventName.equals(menuOp[2])) {
+            showViewReqInfo();
+        }
+
+        if(eventName.equals(menuOp[3])) {
+            showMyReq();
+        }
+
+        if(eventName.equals(menuOp[4])) {
+            modifyReq();
+        }
     }
 }
