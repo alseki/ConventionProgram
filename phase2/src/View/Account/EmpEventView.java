f5ec52e6fc06cfae8b8edcb12bfc1332de6655b4
alsek <allenseung.kim@mail.utoronto.ca>
2020-12-09 19:19:35 -0500

    minor temp comment in EmpEventView

--- phase2/src/View/Account/EmpEventView.java
+++ phase2/src/View/Account/EmpEventView.java
@@ -1,6 +1,7 @@
 package View.Account;
 
 import Presenter.AttendeeController.AttEventController;
+import Presenter.AttendeeController.AttEventMenu;
 import Presenter.Central.SubMenu;
 import Presenter.EmployeeController.EmpEventController;
 import Presenter.EmployeeController.EmpEventMenu;
@@ -10,14 +11,16 @@ import java.awt.event.ActionEvent;
 
     public class EmpEventView extends AccountView {
         EmpEventController controller;
-        EmpEventMenu presenter;
-
+        AttEventMenu presenter;
 
         public EmpEventView(SubMenu controller) {
             super(controller.getPresenter());
             this.controller = (EmpEventController) controller;
             //this.presenter = ((EmpEventController) controller).getPresenter();
+
+            makeDropDownMenu(this.presenter);
         }
+
 
         @Override
         public void actionPerformed(ActionEvent event) {
