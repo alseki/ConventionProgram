b270d7716f1f89ab704e639827d3b8b1195abcfd
alsek <allenseung.kim@mail.utoronto.ca>
2020-12-11 01:15:59 -0500

    corrected uses of "conference" to "convention"

--- phase2/src/Presenter/SpeakerController/SpeEventMenu.java
+++ phase2/src/Presenter/SpeakerController/SpeEventMenu.java
@@ -27,7 +27,7 @@ public class SpeEventMenu extends EventMenu {
 
     @Override
     public String[] getMenuOptions() {
-        return new String[]{"View conference events list", "View your events", "Make an announcement to one of your events",
+        return new String[]{"View convention events list", "View your events", "Make an announcement to one of your events",
                 "Make an announcement to some of your events", "Make an announcement to a list of your events"};
     }
 
